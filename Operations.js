ba9a4e030ca9aaf6787a3096e52f3b7d60a52b40
Carlos López <68704651+CralpCode@users.noreply.github.com>
2022-12-18 22:07:32 +0000

    Agregando Clases

---
+++ Operations.js
@@ -0,0 +1,13 @@
+class Operations{
+
+    constructor (valorx,varlory){
+        this.valorx = 0;
+        this.valory = 0;
+    }
+
+    adition(){
+       let adt = this.valorx + this.valory;
+       return adt;
+    }
+
+}
--- main.js
+++ main.js
@@ -4,20 +4,56 @@ var operationNum = 0;
 var all = "";
 var data = document.getElementById("data");
 var eraser = document.getElementById("eraser");
+var pila = new Pila();
+var operacion = new Operations();
 
 function btns(num) {
+    if (typeof num === "number") {
+        nums += num.toString();
 
-    if (num == "=") {
+        console.log('----tipo num----');
+        console.log('numero');
+        console.log(nums);
+        console.log('--------');
+    }
+
+    if (typeof num === "string" && num != `=`) {
+        pila.push(parseInt(nums));
+        nums = "";
+        operationNum = num;
+        pila.push(num);
+        console.log('----tipo str----')
+        console.log('letra');
+        console.log(num);
+        console.log('--------');
         
     }
 
-    if (num == '+' || num == '-' || num == '/' ||num == '*' ) {
-        all = all + num;
+    
+    if (num != '='){
+        all += num.toString();
         data.innerHTML = all;
     }
+    
+    
+    if (num === '='){
+        console.log('------igual ------');
+        console.log(pila);
+        pila.push(parseInt(nums));
+        operacion.valorx = pila.pila[0];
+        operacion.valory = pila.pila[2];
+        all= operacion.adition();
+        data.innerHTML = all;
+        num = operacion.adition();
+        pila.empty();
+        nums = operacion.adition();
+        console.log(pila);
 
-
+    }
+    
 }
+
+
 
 function scrollleftTop() {
 
