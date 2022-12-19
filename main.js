8c487cd9c5ed82a40c0334f4c03e837f285ae4ba
Carlos López <68704651+CralpCode@users.noreply.github.com>
2022-12-19 18:05:29 +0000

    agregando raiz y potencia

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 var nums = "";
+var convert = 0;
 var numbers = 0;
 var operationNum = 0;
+var guardnum = 0;
+var guardar = [];
 var all = "";
 var data = document.getElementById("data");
 var eraser = document.getElementById("eraser");
 
-function btns(num) {
+function btns(numb) {
+        num = numb.toString();
+    if (num == '√') {
+        return console.log(Math.sqrt(25));
+    }
+
+    if (num == '^') {
+        return console.log(Math.pow(5,2));
+    }
 
     if (num == '=') {
         numbers = eval(all);
@@ -17,6 +28,37 @@ function btns(num) {
         data.innerHTML = all;
         scrollleftTop();
     }
+
+    if (typeof numb === 'number'){
+        nums += numb.toString();
+        guardnum = parseInt(nums); 
+    }
+
+    if (typeof numb === 'string') {
+        console.log('numero guardado = ' + guardnum);
+        guardar.push(guardnum);
+        nums ='';
+        guardbum = 0; 
+        console.log(guardar);
+        console.log('numero borrado = ' + guardnum);
+    }
+
+    /* if (typeof numb === 'string') {
+        console.log('letra')
+    }
+        convert = parseInt(all);
+        let verify = all*1;
+        console.log(verify);
+        if (Number.isNaN(verify)){
+            console.log(convert)
+            let guardar = [];
+            guardar.push(convert);
+            convert = 0;
+        } */
+    
+    
+   
+
 }
 
 function scrollleftTop() {
