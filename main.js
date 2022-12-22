06c1a9a91dea8188f84c7d1e426f221f5e566bea
CralpCode <clp64413@gmail.com>
2022-12-22 13:00:21 -0600

    Completeada

--- main.js
+++ main.js
@@ -10,17 +10,17 @@ var eraser = document.getElementById("eraser");
 
 function btns(entry) {
 
-        num = entry.toString();
+    num = entry.toString();
 
     if (num == '=') {
-        
+
         guardar.push(guardnum);
         convert = operations(guardar);
         data.innerHTML = convert.toString();
         all = convert.toString()
         guardar.length = 0;
-        guardnum = convert;
-        return console.log(guardar);
+        nums = all;
+        return;
     }
 
     else {
@@ -29,122 +29,121 @@ function btns(entry) {
         scrollleftTop();
     }
 
-    if (typeof entry === 'number'){
+    if (typeof entry === 'number') {
         nums += entry.toString();
-        guardnum = parseInt(nums); 
+        guardnum = parseInt(nums);
+        console.log('entrada: ' + guardnum)
+        console.log('numguardado: ' + guardnum)
     }
 
-    if (typeof entry === 'string' ) {
+    if (typeof entry === 'string') {
         guardstring = entry;
         guardar.push(guardnum);
         guardar.push(guardstring);
-        nums ='';
-        guardnum = 0; 
+        nums = '';
+        guardnum = 0;
         console.log(guardar);
     }
-    
+
 }
 
 function operations(list) {
     console.log(guardar);
 
-    for (let index = 0; index < list.length; index++) {
-        if (list[index] == "-" && list[index+1]>0) {
-            let result = -(list[index+1]);
-            list[index+1] = result; 
-            list[list.length] = result; 
+    function reordenar(index, result) {
+        list[index - 1] = result;
+        for (let i = index; i < list.length; i++) {
+            list[i] = list[i + 2]
+        }
+        list.pop();
+        list.pop();
+        list[list.length] = result;
 
-            
-         } 
-        if (list[index] == "^") {
-           let result = Math.pow(list[index-1],list[index+1]);  
-           list[index-1] = result; 
-           for (let i = index; i < list.length; i++) { 
-               list[i] = list[i+2]
+
+    }
+
+    for (let index = 0; index < list.length; index++) {
+        if (list[index] == "res") {
+            let result = list[index - 1] % list[index + 1];
+            reordenar(index, result);
+            index = 0;
+        }
+        if (list[index] == "-" && list[index + 1] > 0) {
+            let result = -(list[index + 1]);
+            list[index + 1] = result;
+            console.log(index + 1)
+
+            if (list[index - 1] == 0) {
+                reordenar(index, result);
+                index = 0;
+                console.log(list)
             }
-            list.pop();
-            list.pop();
-           list[list.length] = result; 
-           console.log(list);
-           
+            console.log(list)
+            console.log(list)
+            list[list.length] = result;
+
+        }
+        if (list[index] == "^") {
+            let result = Math.pow(list[index - 1], list[index + 1]);
+            reordenar(index, result);
+            index = 0;
+            console.log(list);
+
         }
 
         if (list[index] == "√") {
-           let result = Math.sqrt(list[index+1]);
-           console.log(result);
-           if (index > 1) {
-                list[index-1] = result;
-                for (let i = index; i < list.length; i++) { 
-                    list[i] = list[i+2]
-                }
-                list.pop();
-                list.pop();
-                list[list.length] = result;
+            let result = Math.sqrt(list[index + 1]);
+            console.log(result);
+            if (index > 1) {
+                reordenar(index, result);
+                index = 0;
                 //list[index+1] = list[index-1]; 
                 console.log('antes' + list)
                 console.log('despues' + list)
 
-           }
-           
-           list[index-1] = result; 
-           list[list.length] = result;
+            }
+
+            list[index - 1] = result;
+            list[list.length] = result;
         }
-        
+
     }
 
-        for (let index = 0; index < list.length; index++) {
-            if (list[index] == "*") {
-               let result = list[index-1] * (list[index+1]);
-               list[index-1] = result; 
-               for (let i = index; i < list.length; i++) { 
-                   list[i] = list[i+2]
-                }
-                list.pop();
-                list.pop(); 
-                list[list.length] = result;
-               console.log('multi')
-               console.log(list)
-               console.log(result)
-               
-            }
+    for (let index = 0; index < list.length; index++) {
+        if (list[index] == "*") {
+            let result = list[index - 1] * (list[index + 1]);
+            reordenar(index, result);
+            index = 0;
+            console.log('multi')
+            console.log(list)
+            console.log(result)
 
-            if (list[index] == "/") {
-               let result = list[index-1] / list[index+1];
-               list[index-1] = result; 
-               for (let i = index; i < list.length; i++) { 
-                   list[i] = list[i+2]
-                }
-                list.pop();
-                list.pop(); 
-                list[list.length] = result; 
-               console.log('div')
-               console.log(list)
-               
-            }
-            
         }
 
-        for (let index = 0; index < list.length; index++) {           
-            if (list[index] == "+") {
-                let result = list[index-1] + list[index+1];
-                list[index+1] = result; 
-                list[list.length] = result; 
-        
-             }
+        if (list[index] == "/") {
+            let result = list[index - 1] / list[index + 1];
+            reordenar(index, result);
+            index = 0;
+            console.log('div')
+            console.log(list)
 
-             if (list[index] == "-") {
-                let result = list[index-1] +list[index+1];
-                console.log(list.length-1);
-                list[index+1] = result; 
-                list[list.length] = result; 
-                console.log(list);
-                
-             } 
         }
 
-        console.log(guardar);
+    }
 
-    return list[list.length - 1 ];
+    for (let index = 0; index < list.length; index++) {
+        if (list[index] == "+" || list[index] == "-") {
+            let result = list[index - 1] + list[index + 1];
+            list[index + 1] = result;
+            list[list.length] = result;
+
+        }
+
+    }
+
+    console.log(guardar);
+
+    return list[list.length - 1];
 }
 
 function scrollleftTop() {
